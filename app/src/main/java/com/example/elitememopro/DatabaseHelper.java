8cf0e93f5cab05ed3876f412da44bb3e7b46b129
deepanjanyxz <deepanjanbiswas13@gmail.com>
2026-02-12 13:12:40 +0530

    Added DatabaseHelper

---
+++ app/src/main/java/com/example/elitememopro/DatabaseHelper.java
@@ -0,0 +1,79 @@
+package com.example.elitememopro;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+public class DatabaseHelper extends SQLiteOpenHelper {
+
+    private static final String DATABASE_NAME = "EliteMemoPro.db";
+    private static final int DATABASE_VERSION = 1;
+
+    public static final String TABLE_NOTES = "notes";
+    public static final String COLUMN_ID = "_id";
+    public static final String COLUMN_TITLE = "title";
+    public static final String COLUMN_CONTENT = "content";
+    public static final String COLUMN_TIMESTAMP = "timestamp";
+
+    private static final String CREATE_TABLE =
+            "CREATE TABLE " + TABLE_NOTES + " (" +
+                    COLUMN_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                    COLUMN_TITLE + " TEXT NOT NULL, " +
+                    COLUMN_CONTENT + " TEXT NOT NULL, " +
+                    COLUMN_TIMESTAMP + " TEXT NOT NULL" +
+                    ");";
+
+    public DatabaseHelper(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL(CREATE_TABLE);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_NOTES);
+        onCreate(db);
+    }
+
+    public long insertNote(String title, String content) {
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues values = new ContentValues();
+        values.put(COLUMN_TITLE, title);
+        values.put(COLUMN_CONTENT, content);
+        values.put(COLUMN_TIMESTAMP, new SimpleDateFormat("yyyy-MM-dd HH:mm", Locale.getDefault()).format(new Date()));
+        return db.insert(TABLE_NOTES, null, values);
+    }
+
+    public Cursor getAllNotes() {
+        SQLiteDatabase db = this.getReadableDatabase();
+        return db.query(TABLE_NOTES, null, null, null, null, null, COLUMN_TIMESTAMP + " DESC");
+    }
+
+    public int updateNote(int id, String title, String content) {
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues values = new ContentValues();
+        values.put(COLUMN_TITLE, title);
+        values.put(COLUMN_CONTENT, content);
+        values.put(COLUMN_TIMESTAMP, new SimpleDateFormat("yyyy-MM-dd HH:mm", Locale.getDefault()).format(new Date()));
+        return db.update(TABLE_NOTES, values, COLUMN_ID + "=?", new String[]{String.valueOf(id)});
+    }
+
+    public void deleteNote(int id) {
+        SQLiteDatabase db = this.getWritableDatabase();
+        db.delete(TABLE_NOTES, COLUMN_ID + "=?", new String[]{String.valueOf(id)});
+    }
+
+    public void deleteAllNotes() {
+        SQLiteDatabase db = this.getWritableDatabase();
+        db.execSQL("DELETE FROM " + TABLE_NOTES);
+    }
+}

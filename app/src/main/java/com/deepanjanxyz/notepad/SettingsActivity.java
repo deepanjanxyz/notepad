830c9d7731d4aa273f1364a38dad4e9b3fc94ed4
deepanjanyxz <deepanjanbiswas13@gmail.com>
2026-02-12 17:42:00 +0530

    Added Clean Social Media links to About section

--- app/src/main/java/com/deepanjanxyz/notepad/SettingsActivity.java
+++ app/src/main/java/com/deepanjanxyz/notepad/SettingsActivity.java
@@ -1,8 +1,11 @@
 package com.deepanjanxyz.notepad;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.app.AppCompatDelegate;
+import androidx.preference.Preference;
 import androidx.preference.PreferenceFragmentCompat;
 
 public class SettingsActivity extends AppCompatActivity {
@@ -20,7 +23,7 @@ public class SettingsActivity extends AppCompatActivity {
         public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
             setPreferencesFromResource(R.xml.preferences, rootKey);
 
-            // থিম পরিবর্তন লজিক
+            // থিম লজিক
             findPreference("pref_theme").setOnPreferenceChangeListener((preference, newValue) -> {
                 String theme = (String) newValue;
                 if (theme.equals("dark")) AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
@@ -28,6 +31,23 @@ public class SettingsActivity extends AppCompatActivity {
                 else AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM);
                 return true;
             });
+
+            // সোশ্যাল মিডিয়া লিঙ্ক লজিক
+            setupLink("pref_github", "https://github.com/deepanjanxyz/notepad");
+            setupLink("pref_youtube", "https://youtube.com/@shadows-i5o");
+            setupLink("pref_facebook", "https://www.facebook.com/share/1KQcBLkD6K/");
+            setupLink("pref_instagram", "https://www.instagram.com/deepanjan918");
+        }
+
+        private void setupLink(String key, String url) {
+            Preference pref = findPreference(key);
+            if (pref != null) {
+                pref.setOnPreferenceClickListener(preference -> {
+                    Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
+                    startActivity(intent);
+                    return true;
+                });
+            }
         }
     }
 }

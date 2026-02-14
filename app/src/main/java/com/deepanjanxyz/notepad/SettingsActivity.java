c7aa9e181ea4f01ccdf5529d2c4e827ccef65a7b
deepanjanyxz <deepanjanbiswas13@gmail.com>
2026-02-14 16:04:18 +0530

    Removed all YouTube, Instagram, and Facebook references

--- app/src/main/java/com/deepanjanxyz/notepad/SettingsActivity.java
+++ app/src/main/java/com/deepanjanxyz/notepad/SettingsActivity.java
@@ -34,8 +34,6 @@ public class SettingsActivity extends AppCompatActivity {
 
             // সোশ্যাল মিডিয়া লিঙ্ক লজিক
             setupLink("pref_github", "https://github.com/deepanjanxyz/notepad");
-            setupLink("pref_youtube", "https://youtube.com/@shadows-i5o");
-            setupLink("pref_instagram", "https://www.instagram.com/deepanjan918");
         }
 
         private void setupLink(String key, String url) {
